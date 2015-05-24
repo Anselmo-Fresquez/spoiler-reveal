1890a9888bd44dc362aa4c1574b917be307ee38d
Anselmo Fresquez <anselmofresquez@Anselmo-Fresquezs-iMac.local>
2015-05-24 15:44:13 -0700

    Shiny Buttons Look Better Now

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var SpoilerListItems = [];
 for (i = 1; i < spoiler.length; i++)
 {
   SpoilerListItems[i] = $("#spoiler_list_1");
-  $(SpoilerListItems[i]).append("<li data-index=\"" + i + "\" class=\"spoiler\">" + spoiler[i].filmTitle + "</li>");
+  $(SpoilerListItems[i]).append("<li data-index=\"" + i + "\" class=\"button spoiler\"><p class=\"shine\">" + spoiler[i].filmTitle + "</p></li>");
   
 }
 
@@ -12,13 +12,13 @@ $(".spoiler").click(function(){
   var index = $(this).data('index');
   if (spoiler[index].collapsed)
   {
-    $(this).text(spoiler[index].filmTitle + " - " + spoiler[index].spoiler)
+    $(this).children("p").text(spoiler[index].filmTitle + " - " + spoiler[index].spoiler)
     .css("font-style: italic;");
     spoiler[index].collapsed = false;
   }
   else
   {
-    $(this).text(spoiler[index].filmTitle);
+    $(this).children("p").text(spoiler[index].filmTitle);
     spoiler[index].collapsed = true;
   }
 });
