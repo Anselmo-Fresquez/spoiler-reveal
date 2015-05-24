d60ad02d061cf2a67aa9d73393061e0767c52971
Anselmo Fresquez <anselmofresquez@Anselmo-Fresquezs-iMac.local>
2015-05-24 10:29:12 -0700

    Site operational

---
+++ app.js
@@ -0,0 +1,24 @@
+var i;
+var SpoilerListItems = [];
+
+for (i = 1; i < spoiler.length; i++)
+{
+  SpoilerListItems[i] = $("#spoiler_list_1");
+  $(SpoilerListItems[i]).append("<li data-index=\"" + i + "\" class=\"spoiler\">" + spoiler[i].filmTitle + "</li>");
+  
+}
+
+$(".spoiler").click(function(){
+  var index = $(this).data('index');
+  if (spoiler[index].collapsed)
+  {
+    $(this).text(spoiler[index].filmTitle + " - " + spoiler[index].spoiler)
+    .css("font-style: italic;");
+    spoiler[index].collapsed = false;
+  }
+  else
+  {
+    $(this).text(spoiler[index].filmTitle);
+    spoiler[index].collapsed = true;
+  }
+});
---
+++ spoilers.js
@@ -0,0 +1,25 @@
+var spoiler = [];
+
+spoiler[1] = {
+  filmTitle: "Star Wars",
+  spoiler: "Darth Vader is Luke Skywalker's father.",
+  collapsed: true
+};
+
+spoiler[2] = {
+  filmTitle: "The Sixth Sense",
+  spoiler: "The character played by Bruce Willis is dead for the entirety of the film and is not aware of it.",
+  collapsed: true
+};
+
+spoiler[3] = {
+  filmTitle: "Harry Potter and the Half-Blood Prince",
+  spoiler: "Professor Snape kills Dumbledore.",
+  collapsed: true
+};
+
+spoiler[4] = {
+  filmTitle: "The Usual Suspects",
+  spoiler: "Verbal Kint is Keyser Söze.",
+  collapsed: true
+};
